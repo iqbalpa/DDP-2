be103abdb0f4ed014e4f5e1e86a6c7d83fb07860
iqbalpa <iqbalpahlevi07@gmail.com>
2022-02-23 17:36:00 +0700

    fix the problems, only need to convert the method into recursive

--- Lab/Lab 2/ValidasiKodeIMEI.java
+++ Lab/Lab 2/ValidasiKodeIMEI.java
@@ -13,8 +13,18 @@ public class ValidasiKodeIMEI {
      * (harus rekursif), boleh menggunakan helper method
      */
     public static int jumlahDoublePosisiGanjil(long kode) {
-
-        return -1;
+        int total = 0;
+        String kodeStr = Long.toString(kode);
+        for (int i=0; i < kodeStr.length(); i++) {
+            if ( ((i+1)%2) == 1) {
+                int digit = Character.getNumericValue(kodeStr.charAt(i));
+                int newDigit = getDigit(digit);
+                total += newDigit;
+                // System.out.println(newDigit);
+            }
+        }
+        // System.out.println("posisi ganjil: " + total);
+        return total;
     }
 
     /**
@@ -22,11 +32,11 @@ public class ValidasiKodeIMEI {
      * tambahkan dua digit angka tersebut untuk mendapatkan angka yang <= 9.
      */
     public static int getDigit(int number) {
-        int newNumber = number;
-        String numberStr = Integer.toString(number);
+        int newNumber = number * 2;
+        String numberStr = Integer.toString(newNumber);
         if (numberStr.length() == 2) {
-            int digit1 = Integer.valueOf(numberStr.charAt(0));
-            int digit2 = Integer.valueOf(numberStr.charAt(1));
+            int digit1 = Character.getNumericValue(numberStr.charAt(0));
+            int digit2 = Character.getNumericValue(numberStr.charAt(1));
             newNumber = digit1 + digit2;
         }
         return newNumber;
@@ -37,22 +47,34 @@ public class ValidasiKodeIMEI {
      * (harus rekursif), boleh menggunakan helper method
      */
     public static int jumlahPosisiGenap(long kode) {
-
-        return -1;
+        int total = 0;
+        String kodeStr = Long.toString(kode);
+        for (int i=0; i < kodeStr.length(); i++) {
+            if ( ((i+1)%2) == 0) {
+                int digit = Character.getNumericValue(kodeStr.charAt(i));
+                // System.out.println(digit);
+                total += digit;
+            }
+        }
+        // System.out.println("posisi genap: " + total);
+        return total;
     }
 
     /** Return true jika kode merupakan prefix yang valid */
     public static boolean cekPrefix(long kode) {
         String kodeStr = Long.toString(kode);
         if (kodeStr.startsWith("18") || kodeStr.startsWith("2")) {
+            // System.out.println("prefix true");
             return true;
         }
+        // System.out.println("prefix false");
         return false;
     }
 
     /** Return panjang kode */
     public static int getPanjangKode(long kode) {
         String kodeStr = Long.toString(kode);
+        // System.out.println("panjang: " + kodeStr.length());
         return kodeStr.length();
     }
 
